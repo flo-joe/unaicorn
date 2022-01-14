448a576ffc122bf88ff0dae484444b987e015f23
Ubuntu <ubuntu@vm-django-app.ms0smvvg4edernkodnq4fog14a.bx.internal.cloudapp.net>
2022-01-14 19:16:35 +0000

    updated allowed_hosts for server

--- titanic/titanic/settings.py
+++ titanic/titanic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ro7fyvm@*yq2mh9p682z_9+6#wz#p%l-o7pt8q5le$$14@5!ll'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['40.87.1.71']
 
 
 # Application definition
